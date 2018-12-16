7b977ca7cbe4a961637e4034e60de7793356a5d2
Ssunc11 <1392519673@qq.com>
2018-12-16 13:21:07 +0800

    linux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import Button from './Button'
+import './app.css'
+import './'
 class App extends Component {
   render() {
+    const a = 7
+    const title = 'React'
+    console.log(a)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrap">
+        <div>
+          <div className="app" title={title} style={{ color: '#ccc' }}>
+            {' '}
+            hello world {a}{' '}
+          </div>
+          <Button />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Button.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+class Button extends Component {
+  render() {
+    return <button>按钮</button>
+  }
+}
+
+export default Button
