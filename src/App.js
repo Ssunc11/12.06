fd29cb98c95eb1cde0e23ad769ae943760e0f1af
Ssunc11 <1392519673@qq.com>
2018-12-20 15:09:21 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react'
-import Button from './Button'
-import './app.css'
-import './'
+
 class App extends Component {
   render() {
-    const a = 7
-    const title = 'React'
-    console.log(a)
-    return (
-      <div className="wrap">
-        <div>
-          <div className="app" title={title} style={{ color: '#ccc' }}>
-            {' '}
-            hello world {a}{' '}
-          </div>
-          <Button />
-        </div>
-      </div>
-    )
+    return <div>sdsdfgs</div>
   }
 }
 
--- src/Button.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-class Button extends Component {
-  render() {
-    return <button>按钮</button>
-  }
-}
-
-export default Button
